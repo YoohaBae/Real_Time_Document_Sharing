1d2b1c91f5b31dbebcc425e96fd46653cb7b932a
Jason Zheng <jason.zheng.1@stonybrook.edu>
2022-10-28 02:45:54 +0000

    update server directory

--- cse356_backend/index.js
+++ cse356_backend/index.js
@@ -8,6 +8,8 @@ const apiRoutes = require('./routes/api');
 
 app.use(cors());
 
+app.use(express.static('public'))
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -16,6 +18,10 @@ app.use((req, res, next) => {
 })
 
 app.use('/api', apiRoutes);
+
+app.get("/", (req, res) => {
+    res.sendFile("/root/CSE356_Milestones/cse356_backend/start.html")
+})
 
 app.listen(port, () => {
     console.log(`server is listening at localhost:80`);
