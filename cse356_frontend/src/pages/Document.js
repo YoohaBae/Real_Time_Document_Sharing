6e43bef804dab8a76945518cd675524c8d466baf
YoohaBae <yooha.bae.2@gmail.com>
2022-10-27 21:10:22 -0400

    connect frontend with eventsource stream

--- cse356_frontend/src/pages/Document.js
+++ cse356_frontend/src/pages/Document.js
@@ -1,25 +1,31 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import ReactQuill from 'react-quill';
-import urlJoin from "url-join";
-import axios from 'axios';
 import 'react-quill/dist/quill.snow.css';
+import urlJoin from "url-join";
 
 const {REACT_APP_BACKEND_URL} = process.env;
 
 const Document = (props) => {
     let documentID = useParams().id;
-    const establishConnection = () => {
-        return axios.get(urlJoin(
-            REACT_APP_BACKEND_URL + "/api/connect/" + documentID
-        ));
-    }
+
 
     useEffect(() => {
-        establishConnection().then((res) => {
-            console.log(res.data);
-        })
-    })
+        const sse = new EventSource(urlJoin(REACT_APP_BACKEND_URL + "/api/connect/" + documentID));
+        sse.onerror = () => {
+            console.log("error");
+            sse.close();
+        }
+        sse.onopen = () => {
+            console.log("Sse open");
+        }
+        sse.onmessage = (event) => {
+            console.log(event.data)
+        }
+        return () => {
+            sse.close();
+        };
+    }, [documentID])
 
     const [value, setValue] = useState('');
 
