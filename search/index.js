b8b408c9b58d92390f206afacda7afa768363b9c
Jason Zheng <jason.zheng.1@stonybrook.edu>
2022-12-02 03:55:55 +0000

    add user model to search

--- search/index.js
+++ search/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const elasticClient = require("./elasticsearch");
-const User = require('../models/user-model');
+const User = require('./models/user-model');
 const Memcached = require('memcached')
 
 
