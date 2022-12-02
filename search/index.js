e0f5e32e2a987f31551e33b1c7b848eab29faefb
Jason Zheng <jason.zheng.1@stonybrook.edu>
2022-12-02 03:52:57 +0000

    reset path for elasticsearch

--- search/index.js
+++ search/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const elasticClient = require("../elasticsearch");
+const elasticClient = require("./elasticsearch");
 const User = require('../models/user-model');
 const Memcached = require('memcached')
 
